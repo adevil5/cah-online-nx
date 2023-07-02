d70b66a45c00c818ab4763a2e8e9a00995ebfc73
Alex Devillier <alex.devillier@gmail.com>
2023-07-01 19:25:32 -0500

    supabase lib init

---
+++ libs/cah-db/src/lib/cah-db.spec.ts
@@ -0,0 +1,7 @@
+import { cahDb } from './cah-db';
+
+describe('cahDb', () => {
+  it('should work', () => {
+    expect(cahDb()).toEqual('cah-db');
+  });
+});
---
+++ libs/cah-db/src/lib/cah-db.ts
@@ -0,0 +1,3 @@
+export function cahDb(): string {
+  return 'cah-db';
+}
