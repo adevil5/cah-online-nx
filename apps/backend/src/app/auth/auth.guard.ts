46528235c8875ac1a1a296a288e8cd923592c6fe
Alex Devillier <alex.devillier@gmail.com>
2024-01-19 11:03:10 -0600

    Update auth.guard.ts and tsconfig.base.json

--- apps/backend/src/app/auth/auth.guard.ts
+++ apps/backend/src/app/auth/auth.guard.ts
@@ -1,17 +1,33 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+
+class DecodedJwt {
+  sub: string;
+  name?: string;
+  iat?: number;
+  exp?: number;
+  role?: string;
+  aud?: string;
+  iss?: string;
+  jti?: string;
+}
+
+export interface AuthenticatedRequest extends Request {
+  user: DecodedJwt;
+}
 
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(private jwtService: JwtService) {}
 
   canActivate(context: ExecutionContext): boolean {
-    const request = context.switchToHttp().getRequest();
+    const request: AuthenticatedRequest = context.switchToHttp().getRequest();
     const token = request.headers.authorization?.split(' ')[1];
     if (!token) return false;
 
     try {
-      const decoded = this.jwtService.verify(token, {
+      const decoded: DecodedJwt = this.jwtService.verify(token, {
         secret: process.env.SUPABASE_JWT_SECRET,
       });
       request.user = decoded;
